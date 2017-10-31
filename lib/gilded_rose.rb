7e3c1e19eebbfa2eefe08c8c46e365fccf094231
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-31 14:35:21 +0000

    Got rid of convoluted check for what type of object a thing is, as aspects of Quality handle this

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -10,14 +10,13 @@ class GildedRose
       if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert') # excludes Aged Brie from quality reductiondue to special date increase of quality; excludes pass from quality reduction due to special date increase of quality
 
         if item.quality.quality > 0 # Checks that item cannot go into negative quality
-          if item.name != 'Sulfuras, Hand of Ragnaros' # Checks that item is not a special type that can never lose quality (true/false status)
+
             item.quality.update
-          end
+
         end
 
       else # special zproperty rules
         item.quality.update if item.quality.quality < 50 # items are limited at a quality of 50
-
 
           if item.name == 'Backstage passes to a TAFKAL80ETC concert' # checks if pass, and institutes rules for it
 
@@ -41,24 +40,24 @@ class GildedRose
         item.sell_in = item.sell_in - 1 # decreases sell_by_date
       end
 
-      if item.sell_in < 0 # checks if expiration date has passed
-        if item.name != 'Aged Brie' # brie is special; do not degrade
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert' # passes are special and must nto degrade
-            if item.quality > 0 # checks if quality hasn't dropped below zero
-              if item.name != 'Sulfuras, Hand of Ragnaros' # cehcks not specialsulfuras which never degrades
-                item.quality = item.quality - 1 # reduces quality
-              end
-            end
-          else
-            item.quality = item.quality - item.quality # deletes quality of concert pass
-          end
-        else
-          if item.quality < 50 # checks not less than 50
-            item.quality = item.quality + 1 # normal item quality enchancer
-          end
-        end
-
-      end
+      # if item.sell_in < 0 # checks if expiration date has passed
+      #   if item.name != 'Aged Brie' # brie is special; do not degrade
+      #     if item.name != 'Backstage passes to a TAFKAL80ETC concert' # passes are special and must nto degrade
+      #       if item.quality > 0 # checks if quality hasn't dropped below zero
+      #         if item.name != 'Sulfuras, Hand of Ragnaros' # cehcks not specialsulfuras which never degrades
+      #           item.quality = item.quality - 1 # reduces quality
+      #         end
+      #       end
+      #     else
+      #       item.quality = item.quality - item.quality # deletes quality of concert pass
+      #     end
+      #   else
+      #     if item.quality < 50 # checks not less than 50
+      #       item.quality = item.quality + 1 # normal item quality enchancer
+      #     end
+      #   end
+      #
+      # end
     end
   end
 end
