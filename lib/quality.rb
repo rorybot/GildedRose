cb88c426570f063a9d311210f13eb882d7938d78
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-31 11:44:24 +0000

    GREEN: Quality class base attribute can be increased

--- lib/quality.rb
+++ lib/quality.rb
@@ -1,4 +1,11 @@
 class Quality
 
+  def initialize(quality)
+    @quality = quality
+  end
+
+  def increase_quality(amount)
+    @quality +=(amount)
+  end
 
 end
--- spec/quality_spec.rb
+++ spec/quality_spec.rb
@@ -3,8 +3,8 @@ require 'quality'
 describe Quality do
   describe 'base quality property' do
     it 'can be increased' do
-      a_quality = Quality.new
-      expect(a_quality.increase).to eq (2)
+      a_quality = Quality.new(0)
+      expect(a_quality.increase_quality(2)).to eq (2)
     end
   end
 end
