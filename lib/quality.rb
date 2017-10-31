d1d27cee7e28603329b8ab5dfb488a3fe854a271
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-31 12:04:41 +0000

    Item Spec expects quality object

--- lib/quality.rb
+++ lib/quality.rb
@@ -2,6 +2,7 @@ class Quality
 
   def initialize(quality)
     @quality = quality
+    return @quality
   end
 
   def change_quality(amount)
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,10 +2,15 @@ require 'item'
 
 describe Item do
 
+  let(:test_quality) do
+    double('Quality', quality: 50, change_quality_50_by_2: 48)
+  end
+
   describe 'item initialisation' do
     it 'can print name' do
-      new_item = Item.new('Thing', 0, 0)
-      expect(new_item.to_s).to eq "Thing, 0, 0"
+      new_item = Item.new('Thing', 0, test_quality.quality)
+      expect(new_item.to_s).to eq "Thing, 0, 50"
     end
+
   end
 end
