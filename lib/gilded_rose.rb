8fd7eaf1fe056239047dc5c3a1b4319b40519ceb
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-31 14:38:26 +0000

    Added sell-ins to tests

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,55 +9,30 @@ class GildedRose
     @items.each do |item| # iterates over every item
       if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert') # excludes Aged Brie from quality reductiondue to special date increase of quality; excludes pass from quality reduction due to special date increase of quality
 
-        if item.quality.quality > 0 # Checks that item cannot go into negative quality
-
-            item.quality.update
-
-        end
+        item.quality.update if item.quality.quality > 0 # Checks that item cannot go into negative quality
 
       else # special zproperty rules
         item.quality.update if item.quality.quality < 50 # items are limited at a quality of 50
 
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert' # checks if pass, and institutes rules for it
+        if item.name == 'Backstage passes to a TAFKAL80ETC concert' # checks if pass, and institutes rules for it
 
-            if item.sell_in < 11 # special quality rules for when it is less than ten days till expiration date, but more than 5
-               item.quality.update if item.quality.quality < 50 # checs again that quality of item is still not 50
-                 # increases quality
-            end
-
-            if item.sell_in < 6 # special qialuty rules for when less than 6 days left before expiration date
-              item.quality.update if item.quality.quality < 50 # checks again taht quality of item is still not 50
-                 # increases quality (Needs something to drop it to zero though)
-
-            end
-
+          if item.sell_in < 11 # special quality rules for when it is less than ten days till expiration date, but more than 5
+            item.quality.update if item.quality.quality < 50 # checs again that quality of item is still not 50
+            # increases quality
           end
 
-
+          if item.sell_in < 6 # special qialuty rules for when less than 6 days left before expiration date
+            item.quality.update if item.quality.quality < 50 # checks again taht quality of item is still not 50
+            # increases quality (Needs something to drop it to zero though)
+          end
+        end
       end
 
-      if item.name != 'Sulfuras, Hand of Ragnaros' # checks not Sulfuras, which never degrase (true/false)
-        item.sell_in = item.sell_in - 1 # decreases sell_by_date
-      end
 
-      # if item.sell_in < 0 # checks if expiration date has passed
-      #   if item.name != 'Aged Brie' # brie is special; do not degrade
-      #     if item.name != 'Backstage passes to a TAFKAL80ETC concert' # passes are special and must nto degrade
-      #       if item.quality > 0 # checks if quality hasn't dropped below zero
-      #         if item.name != 'Sulfuras, Hand of Ragnaros' # cehcks not specialsulfuras which never degrades
-      #           item.quality = item.quality - 1 # reduces quality
-      #         end
-      #       end
-      #     else
-      #       item.quality = item.quality - item.quality # deletes quality of concert pass
-      #     end
-      #   else
-      #     if item.quality < 50 # checks not less than 50
-      #       item.quality = item.quality + 1 # normal item quality enchancer
-      #     end
-      #   end
-      #
-      # end
+      item.sell_in = item.sell_in - 1 # decreases sell_by_date
+
+
+
     end
   end
 end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -7,12 +7,14 @@ describe GildedRose do
       items = [Item.new('foo', 10, Quality.new(10))]
       GildedRose.new(items).update_quality
       expect(items[0].name).to eq 'foo'
+      expect(items[0].sell_in).to eq 9
     end
 
     it 'reduces by 1' do
       items = [Item.new('foo', 10, Quality.new(10))]
       GildedRose.new(items).update_quality
       expect(items[0].quality.quality).to eq 9
+      expect(items[0].sell_in).to eq 9
     end
 
     # it 'increase quality' do
@@ -25,18 +27,21 @@ describe GildedRose do
         items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 5, Quality.new(10, false, true))]
         GildedRose.new(items).update_quality
         expect(items[0].quality.quality).to eq 13
+        expect(items[0].sell_in).to eq 4
       end
 
       it 'increase ticket price by 2 when 10 days left' do
         items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 10, Quality.new(10, false, true))]
         GildedRose.new(items).update_quality
         expect(items[0].quality.quality).to eq 12
+        expect(items[0].sell_in).to eq 9
       end
 
       it 'increase ticket price by 1 when 11 days left' do
         items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 11, Quality.new(10, false, true))]
         GildedRose.new(items).update_quality
         expect(items[0].quality.quality).to eq 11
+        expect(items[0].sell_in).to eq 10
       end
     end
 
@@ -45,6 +50,7 @@ describe GildedRose do
         items = [Item.new('foo', 10, Quality.new(30,true))]
         GildedRose.new(items).update_quality
         expect(items[0].quality.quality).to eq 30
+        expect(items[0].sell_in).to eq 9
       end
     end
 
@@ -53,6 +59,7 @@ describe GildedRose do
         items = [Item.new('foo', 10, Quality.new(30,false,true))]
         GildedRose.new(items).update_quality
         expect(items[0].quality.quality).to eq 31
+        expect(items[0].sell_in).to eq 9
       end
     end
   end
