06c3d71ece62220029538754ec352c88796d1a36
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-31 12:21:56 +0000

    RED: initialise sellby dates class object

---
+++ spec/date_spec.rb
@@ -0,0 +1,11 @@
+require 'date'
+
+describe SellBy do
+
+  describe 'sellby initialisation' do
+    it 'has a sellby date' do
+      expect(SellBy.new("20/11/2017").expires).to eq "20/11/2017"
+    end
+  end
+
+end
