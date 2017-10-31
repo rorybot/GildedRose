d1cd76842f266f532830a410034b3cba576b3416
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-31 12:41:41 +0000

    RED: grower objects increase in quality when updated; refactor to update method

--- lib/quality.rb
+++ lib/quality.rb
@@ -1,11 +1,16 @@
 class Quality
-  def initialize(quality, immortal = false)
+
+  attr_reader :immortal
+  def initialize(quality, immortal=false, grower=false)
     @quality = quality
     @immortal = immortal
-    @quality
+    @grower = grower
+    return @quality
   end
 
+
   def change_quality(amount)
-    return @quality += amount unless @immortal == true
+    return @quality += amount unless immortal == true
+    "That which is dead can never die"
   end
 end
--- spec/quality_spec.rb
+++ spec/quality_spec.rb
@@ -12,12 +12,23 @@ describe Quality do
       a_quality.change_quality(2)
       expect(a_quality.change_quality(-1)).to eq 1
     end
+
+    it 'cannot go past 50' do
+    end
   end
 
   describe 'immortal objects' do
     it 'cannot be changed' do
-      immortal_quality = Quality.new(50)
-      expect(a_quality.change_quality(-1))
+      immortal_quality = Quality.new(50, true)
+      expect(immortal_quality.change_quality(-1)).to eq "That which is dead can never die"
     end
   end
+
+  describe 'growing quality objects' do
+    it 'increases in quality if a grower' do
+      grower_quality = Quality.new(30)
+      expect(grower_quality.update).to eq(31)
+    end
+  end
+
 end
