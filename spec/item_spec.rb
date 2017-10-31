a9179e156cf3558c0f8c18131415dd67357b6821
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-31 12:17:22 +0000

    Fixed formatting

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,7 +1,6 @@
 require 'item'
 
 describe Item do
-
   let(:test_quality) do
     double('Quality', quality: 50, change_quality_50_by_2: 48)
   end
@@ -11,6 +10,5 @@ describe Item do
       new_item = Item.new('Thing', 0, test_quality.quality)
       expect(new_item.to_s).to eq "Thing, 0, 50"
     end
-
   end
 end
