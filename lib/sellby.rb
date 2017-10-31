727f3dffb5ef5a56760ecc143be4a2416ef76883
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-31 15:53:53 +0000

    Removed expectation that sellby would know of object state

--- lib/sellby.rb
+++ lib/sellby.rb
@@ -1,9 +1,8 @@
 class SellBy
   attr_reader :days_left, :special_object
 
-  def initialize(days, special_object = false)
+  def initialize(days)
     @days_left = days
-    @special_object = special_object
   end
 
   def update
@@ -11,14 +10,8 @@ class SellBy
   end
 
   def modifier
-    return 1 if backstage_pass? && days_left >= 11
-    return 2 if backstage_pass? && days_left < 11 && days_left >= 6
-    return 3 if backstage_pass? && days_left < 6 && days_left > 0
+    return 1 if days_left >= 11
+    return 2 if days_left < 11 && days_left >= 6
+    return 3 if days_left < 6 && days_left > 0
   end
-
-  def backstage_pass?
-    special_object == 'Backstage passes to a TAFKAL80ETC concert'
-  end
-
-
 end
--- spec/date_spec.rb
+++ spec/date_spec.rb
@@ -6,37 +6,24 @@ describe SellBy do
       expect(SellBy.new(10).days_left).to eq 10
     end
 
-    it 'can be a special object' do
-      expect(SellBy.new(10, 'Backstage passes to a TAFKAL80ETC concert').special_object).to eq 'Backstage passes to a TAFKAL80ETC concert'
-    end
-
     it 'can lower days left' do
       selly = SellBy.new(10)
       expect(selly.update).to eq 9
     end
 
     it 'modifier returns 1 if 11 days left of backstage pass' do
-      selly = SellBy.new(4, 'Backstage passes to a TAFKAL80ETC concert')
+      selly = SellBy.new(4)
       expect(selly.modifier).to eq 3
     end
 
     it 'modifier returns 2 if 7 days left of backstage pass' do
-      selly = SellBy.new(7, 'Backstage passes to a TAFKAL80ETC concert')
+      selly = SellBy.new(7)
       expect(selly.modifier).to eq 2
     end
 
     it 'modifier returns 3 if 4 days left of backstage pass' do
-      selly = SellBy.new(4, 'Backstage passes to a TAFKAL80ETC concert')
+      selly = SellBy.new(4)
       expect(selly.modifier).to eq 3
     end
-
-    it 'modifier no modifier if 7 days left of normal object' do
-      selly = SellBy.new(7)
-      expect(selly.modifier).to eq nil
-    end
-
-
-
   end
-
 end
