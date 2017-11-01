e81e4cc58f5cee68cee1ff12cc7cd4b5ff3e5f9a
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-11-01 08:40:21 +0000

    Explicated normal update functions out to own private methods with more obvious names

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -10,13 +10,13 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      item.quality.update unless special_items.include?(item.name)
+      normal_update(item)
 
       item.quality.flatline if item.sell_in.days_left <= 0 && item.name == 'Backstage passes to a TAFKAL80ETC concert'
 
       pass_modifier(item.sell_in).times { item.quality.update } if item.name == 'Backstage passes to a TAFKAL80ETC concert'
 
-      item.sell_in.update
+      one_day_less(item)
     end
   end
 
@@ -24,6 +24,16 @@ class GildedRose
     return 1 if sell_by_date.days_left >= 11 || sell_by_date.days_left < 0
     return 2 if sell_by_date.days_left < 11 && sell_by_date.days_left >= 6
     return 3 if sell_by_date.days_left < 6 && sell_by_date.days_left >= 0
+  end
+
+  private
+
+  def normal_update(item)
+    item.quality.update unless special_items.include?(item.name)
+  end
+
+  def one_day_less(item)
+    item.sell_in.update
   end
 
 
