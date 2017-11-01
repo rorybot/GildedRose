10e46edbdfd6ece57eeb5d22d7500a391ee7b87c
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-11-01 12:03:24 +0000

    Removed special-item check; redundant

--- lib/name.rb
+++ lib/name.rb
@@ -2,15 +2,10 @@ class ItemName
   attr_reader :name, :special_items
   def initialize(name)
     @name = name
-    @special_items = ['Backstage passes to a TAFKAL80ETC concert']
   end
 
   def backstage_pass?
     name == 'Backstage passes to a TAFKAL80ETC concert'
-  end
-
-  def special_item?
-    special_items.include?(name)
   end
 
   def to_s
