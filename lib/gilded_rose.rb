baf325ba9246152cbf7d6105ae8d7a51cbd4d1a7
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-11-01 08:28:09 +0000

    Pushed special item status check to array, for adding in conjured item later

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,13 +1,16 @@
   require_relative 'item'
 
 class GildedRose
+  attr_reader :special_items
+
   def initialize(items)
     @items = items
+    @special_items = ['Backstage passes to a TAFKAL80ETC concert']
   end
 
   def update_quality
     @items.each do |item|
-      item.quality.update if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+      item.quality.update unless special_items.include?(item.name)
 
       item.quality.flatline if item.sell_in.days_left <= 0 && item.name == 'Backstage passes to a TAFKAL80ETC concert'
 
@@ -16,4 +19,5 @@ class GildedRose
       item.sell_in.update
     end
   end
+
 end
