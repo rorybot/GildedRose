bba57bea7fc0a4114af92af73f16b9be878c6f00
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-11-01 08:46:23 +0000

    Removed unnecessary check for flatlining value of object when expired

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,4 +1,4 @@
-  require_relative 'item'
+require_relative 'item'
 
 class GildedRose
   attr_reader :special_items
@@ -12,18 +12,12 @@ class GildedRose
     @items.each do |item|
       normal_update(item)
 
-      item.quality.flatline if item.sell_in.days_left <= 0 && item.name == 'Backstage passes to a TAFKAL80ETC concert'
+      remove_all_value_when_expired(item)
 
       pass_modifier(item.sell_in).times { item.quality.update } if item.name == 'Backstage passes to a TAFKAL80ETC concert'
 
       one_day_less(item)
     end
-  end
-
-  def pass_modifier(sell_by_date)
-    return 1 if sell_by_date.days_left >= 11 || sell_by_date.days_left < 0
-    return 2 if sell_by_date.days_left < 11 && sell_by_date.days_left >= 6
-    return 3 if sell_by_date.days_left < 6 && sell_by_date.days_left >= 0
   end
 
   private
@@ -36,5 +30,13 @@ class GildedRose
     item.sell_in.update
   end
 
+  def remove_all_value_when_expired(item)
+    item.quality.flatline if item.sell_in.days_left <= 0
+  end
 
-end
+  def pass_modifier(sell_by_date)
+    return 1 if sell_by_date.days_left >= 11 || sell_by_date.days_left < 0
+    return 2 if sell_by_date.days_left < 11 && sell_by_date.days_left >= 6
+    return 3 if sell_by_date.days_left < 6 && sell_by_date.days_left >= 0
+  end
+  end
